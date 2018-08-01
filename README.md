f6550488ed4a1224c4d2df644abb558296209635
Jeny Alejandra Mazo <yenymzo@gmail.com>
2018-08-01 14:25:53 -0500

    creating alignable categories

---
+++ README.md
@@ -0,0 +1,1 @@
+# alignable_categories
---
+++ index.js
@@ -0,0 +1,852 @@
+'use strict';
+
+exports.categories = [
+  {
+    id: 1,
+    title: 'Fashion Design'
+  },
+  {
+    id: 2,
+    title: 'Performing Arts'
+  },
+  {
+    id: 3,
+    title: 'Print, Radio and Television Production'
+  },
+  {
+    id: 4,
+    title: 'Printing and Publishing'
+  },
+  {
+    id: 5,
+    title: 'Screenprinting and Embroidery'
+  },
+  {
+    id: 6,
+    title: 'Detail and Carwash'
+  },
+  {
+    id: 7,
+    title: 'Appraisers'
+  },
+  {
+    id: 8,
+    title: 'Consultants'
+  },
+  {
+    id: 9,
+    title: 'Product Design Services'
+  },
+  {
+    id: 10,
+    title: 'Payroll Services'
+  },
+  {
+    id: 11,
+    title: 'Recruiting and Staffing Services'
+  },
+  {
+    id: 12,
+    title: 'Recycling Services'
+  },
+  {
+    id: 13,
+    title: 'Web Design, Graphic Design, and Audio'
+  },
+  {
+    id: 14,
+    title: 'Employment Agency'
+  },
+  {
+    id: 15,
+    title: 'Economic Development'
+  },
+  {
+    id: 16,
+    title: 'Water Filtration & Treatment'
+  },
+  {
+    id: 17,
+    title: 'Blasting and Demolition'
+  },
+  {
+    id: 18,
+    title: 'Government'
+  },
+  {
+    id: 19,
+    title: 'Library'
+  },
+  {
+    id: 20,
+    title: 'Early Childhood Education'
+  },
+  {
+    id: 21,
+    title: 'Educational Resources'
+  },
+  {
+    id: 22,
+    title: 'Bed and Breakfast'
+  },
+  {
+    id: 23,
+    title: 'Hotel, Motel'
+  },
+  {
+    id: 24,
+    title: 'Acupuncture'
+  },
+  {
+    id: 25,
+    title: 'Animal Hospital'
+  },
+  {
+    id: 26,
+    title: 'Assisted Living and Senior Care'
+  },
+  {
+    id: 27,
+    title: 'Audiologists'
+  },
+  {
+    id: 28,
+    title: 'Chiropractors'
+  },
+  {
+    id: 29,
+    title: 'Clinics, Hospitals and Medical Centers'
+  },
+  {
+    id: 30,
+    title: 'Counseling Services'
+  },
+  {
+    id: 31,
+    title: 'Dental'
+  },
+  {
+    id: 32,
+    title: 'Diet and Nutrition'
+  },
+  {
+    id: 33,
+    title: 'Doctors, Physicians and Assistants'
+  },
+  {
+    id: 34,
+    title: 'Laboratory, Imaging and Diagnostic'
+  },
+  {
+    id: 35,
+    title: 'Pharmacy, Drug and Vitamin Stores'
+  },
+  {
+    id: 36,
+    title: 'Physical Therapy'
+  },
+  {
+    id: 37,
+    title: 'Podiatrist'
+  },
+  {
+    id: 38,
+    title: 'Psychiatrist'
+  },
+  {
+    id: 39,
+    title: 'Psychological Services'
+  },
+  {
+    id: 40,
+    title: 'Therapists'
+  },
+  {
+    id: 41,
+    title: 'Social Worker'
+  },
+  {
+    id: 42,
+    title: 'Veterinarian'
+  },
+  {
+    id: 43,
+    title: 'Venue Space'
+  },
+  {
+    id: 44,
+    title: 'Chamber of Commerce'
+  },
+  {
+    id: 45,
+    title: 'Local First'
+  },
+  {
+    id: 46,
+    title: 'Organization'
+  },
+  {
+    id: 47,
+    title: 'Social Clubs'
+  },
+  {
+    id: 48,
+    title: 'Main Street'
+  },
+  {
+    id: 49,
+    title: 'Arborists'
+  },
+  {
+    id: 50,
+    title: 'Environmental Assessments'
+  },
+  {
+    id: 51,
+    title: 'Garden Center/nursery'
+  },
+  {
+    id: 52,
+    title: 'Landscape and Lawn Service'
+  },
+  {
+    id: 53,
+    title: 'Solar'
+  },
+  {
+    id: 54,
+    title: 'Farming and Agriculture'
+  },
+  {
+    id: 55,
+    title: 'Audio/video Services'
+  },
+  {
+    id: 56,
+    title: 'Business Services'
+  },
+  {
+    id: 57,
+    title: 'Computer Programming, Software and Support'
+  },
+  {
+    id: 58,
+    title: 'Electricians'
+  },
+  {
+    id: 59,
+    title: 'Entertainers, Actors, and Directors'
+  },
+  {
+    id: 60,
+    title: 'Financial Services'
+  },
+  {
+    id: 61,
+    title: 'Furnace Service and Repair'
+  },
+  {
+    id: 62,
+    title: 'Garage Door Repair'
+  },
+  {
+    id: 63,
+    title: 'Handyman'
+  },
+  {
+    id: 64,
+    title: 'Hvac'
+  },
+  {
+    id: 65,
+    title: 'Inspection Services'
+  },
+  {
+    id: 66,
+    title: 'Exterminators'
+  },
+  {
+    id: 67,
+    title: 'Insurance'
+  },
+  {
+    id: 68,
+    title: 'Legal Services'
+  },
+  {
+    id: 69,
+    title: 'Locksmith'
+  },
+  {
+    id: 70,
+    title: 'Painters'
+  },
+  {
+    id: 71,
+    title: 'Pest Control'
+  },
+  {
+    id: 72,
+    title: 'Photography and Videography'
+  },
+  {
+    id: 73,
+    title: 'Plumbers'
+  },
+  {
+    id: 74,
+    title: 'Professional Services'
+  },
+  {
+    id: 75,
+    title: 'Roofers'
+  },
+  {
+    id: 76,
+    title: 'Security System and Services'
+  },
+  {
+    id: 77,
+    title: 'Tailors'
+  },
+  {
+    id: 78,
+    title: 'Tax Preparation'
+  },
+  {
+    id: 79,
+    title: 'Apartments and Home Rentals'
+  },
+  {
+    id: 80,
+    title: 'Cleaning and Janitorial Services'
+  },
+  {
+    id: 81,
+    title: 'Construction Companies'
+  },
+  {
+    id: 82,
+    title: 'Contractors'
+  },
+  {
+    id: 83,
+    title: 'Interior Design'
+  },
+  {
+    id: 84,
+    title: 'Property and Facilities Management'
+  },
+  {
+    id: 85,
+    title: 'Real Estate'
+  },
+  {
+    id: 86,
+    title: 'Rental and Leasing'
+  },
+  {
+    id: 87,
+    title: 'Title Company'
+  },
+  {
+    id: 88,
+    title: 'Real Estate Builders'
+  },
+  {
+    id: 89,
+    title: 'Alcohol and Tobacco'
+  },
+  {
+    id: 90,
+    title: 'Antiques and Collectibles'
+  },
+  {
+    id: 91,
+    title: 'Jewelry'
+  },
+  {
+    id: 92,
+    title: 'Packaging and Shipping'
+  },
+  {
+    id: 93,
+    title: 'Pet Care and Supplies'
+  },
+  {
+    id: 94,
+    title: 'Retail/department Stores'
+  },
+  {
+    id: 95,
+    title: 'Shoes'
+  },
+  {
+    id: 96,
+    title: 'Signs'
+  },
+  {
+    id: 97,
+    title: 'Speciality Retail'
+  },
+  {
+    id: 98,
+    title: 'Sporting Goods'
+  },
+  {
+    id: 99,
+    title: 'Barbers, Hairstyling, Spas and Beauty Salons'
+  },
+  {
+    id: 100,
+    title: 'Tattoos and Body Piercing Studios'
+  },
+  {
+    id: 101,
+    title: 'Country Clubs'
+  },
+  {
+    id: 102,
+    title: 'Dance and Performing Arts Studios'
+  },
+  {
+    id: 103,
+    title: 'Exercise and Fitness, Gyms'
+  },
+  {
+    id: 104,
+    title: 'Sports and Recreation'
+  },
+  {
+    id: 105,
+    title: 'Building Materials and Supplies'
+  },
+  {
+    id: 106,
+    title: 'Distribution, Import and Export'
+  },
+  {
+    id: 107,
+    title: 'Equipment Suppliers'
+  },
+  {
+    id: 108,
+    title: 'Coach'
+  },
+  {
+    id: 109,
+    title: 'Gas Stations'
+  },
+  {
+    id: 110,
+    title: 'Towing'
+  },
+  {
+    id: 111,
+    title: 'Transportation'
+  },
+  {
+    id: 112,
+    title: 'Travel and Tourism'
+  },
+  {
+    id: 113,
+    title: 'Zoos'
+  },
+  {
+    id: 114,
+    title: 'Other'
+  },
+  {
+    id: 115,
+    title: 'Aquariums'
+  },
+  {
+    id: 116,
+    title: 'Amusement Parks'
+  },
+  {
+    id: 117,
+    title: 'Movie Theaters'
+  },
+  {
+    id: 118,
+    title: 'Artists and Writers'
+  },
+  {
+    id: 119,
+    title: 'Galleries and Museums'
+  },
+  {
+    id: 120,
+    title: 'Media'
+  },
+  {
+    id: 121,
+    title: 'Music and Recording Studios'
+  },
+  {
+    id: 122,
+    title: 'Music Lessons'
+  },
+  {
+    id: 123,
+    title: 'Musicians'
+  },
+  {
+    id: 124,
+    title: 'Auto Accessories'
+  },
+  {
+    id: 125,
+    title: 'Auto Dealers'
+  },
+  {
+    id: 126,
+    title: 'Auto Service, Repair and Parts'
+  },
+  {
+    id: 127,
+    title: 'Auto Body Shops'
+  },
+  {
+    id: 128,
+    title: 'Parking Garages'
+  },
+  {
+    id: 129,
+    title: 'Marketing, Advertising and Communication'
+  },
+  {
+    id: 130,
+    title: 'Design Services'
+  },
+  {
+    id: 131,
+    title: 'Business Coaching'
+  },
+  {
+    id: 132,
+    title: 'Web Design and Development'
+  },
+  {
+    id: 133,
+    title: 'Design Agency'
+  },
+  {
+    id: 134,
+    title: 'Human Resources'
+  },
+  {
+    id: 135,
+    title: 'Computer Repairs and Services'
+  },
+  {
+    id: 136,
+    title: 'Computer Consulting Services and Support'
+  },
+  {
+    id: 137,
+    title: 'Telecommunication'
+  },
+  {
+    id: 138,
+    title: 'Sales Training'
+  },
+  {
+    id: 139,
+    title: 'Bookkeeping'
+  },
+  {
+    id: 140,
+    title: 'Manufacturing'
+  },
+  {
+    id: 141,
+    title: 'Plaster, Asphalt and Concrete Services'
+  },
+  {
+    id: 142,
+    title: 'Contaminant Removal'
+  },
+  {
+    id: 143,
+    title: 'Civic Groups and Communities'
+  },
+  {
+    id: 144,
+    title: 'Adult and Continuing Education'
+  },
+  {
+    id: 145,
+    title: 'Universities and Schools'
+  },
+  {
+    id: 146,
+    title: 'Visual Art and Design Classes'
+  },
+  {
+    id: 147,
+    title: 'Schools'
+  },
+  {
+    id: 148,
+    title: 'Event Planners and Supplies'
+  },
+  {
+    id: 149,
+    title: 'Bank'
+  },
+  {
+    id: 150,
+    title: 'Bakeries and Desserts'
+  },
+  {
+    id: 151,
+    title: 'Cafe'
+  },
+  {
+    id: 152,
+    title: 'Catering and Supplies'
+  },
+  {
+    id: 153,
+    title: 'Farm Stand'
+  },
+  {
+    id: 154,
+    title: 'Food Supplier'
+  },
+  {
+    id: 155,
+    title: 'Grocery Store'
+  },
+  {
+    id: 156,
+    title: 'Bars and Clubs'
+  },
+  {
+    id: 157,
+    title: 'Fast Food and Carry Out'
+  },
+  {
+    id: 158,
+    title: 'Restaurant'
+  },
+  {
+    id: 159,
+    title: 'Chimney Sweeping'
+  },
+  {
+    id: 160,
+    title: 'Home Furnishings, and Goods'
+  },
+  {
+    id: 161,
+    title: 'Home Improvements and Repairs'
+  },
+  {
+    id: 162,
+    title: 'Attorneys/lawyers'
+  },
+  {
+    id: 163,
+    title: 'Childcare/day Care'
+  },
+  {
+    id: 164,
+    title: 'Errand Services'
+  },
+  {
+    id: 165,
+    title: 'Funeral Service Providers and Cemeteries'
+  },
+  {
+    id: 166,
+    title: 'Wedding Services'
+  },
+  {
+    id: 167,
+    title: 'Dog Sitting'
+  },
+  {
+    id: 168,
+    title: 'Pet Sitting'
+  },
+  {
+    id: 169,
+    title: 'Play School'
+  },
+  {
+    id: 170,
+    title: 'Tutors'
+  },
+  {
+    id: 171,
+    title: 'Nursery'
+  },
+  {
+    id: 172,
+    title: 'Massage Therapy'
+  },
+  {
+    id: 173,
+    title: 'Medical Services'
+  },
+  {
+    id: 174,
+    title: 'Medical Supplies'
+  },
+  {
+    id: 175,
+    title: 'Mental Health'
+  },
+  {
+    id: 176,
+    title: 'Nurse'
+  },
+  {
+    id: 177,
+    title: 'Wellness'
+  },
+  {
+    id: 178,
+    title: 'Physiotherapists'
+  },
+  {
+    id: 179,
+    title: 'Non Profit Organization'
+  },
+  {
+    id: 180,
+    title: 'School Groups or Organizations'
+  },
+  {
+    id: 181,
+    title: 'Accountants'
+  },
+  {
+    id: 182,
+    title: 'Miscellaneous Services'
+  },
+  {
+    id: 183,
+    title: 'Moving and Storage'
+  },
+  {
+    id: 184,
+    title: 'Welders'
+  },
+  {
+    id: 185,
+    title: 'Pool Supplies and Services'
+  },
+  {
+    id: 186,
+    title: 'Carpet Cleaning'
+  },
+  {
+    id: 187,
+    title: 'Architects, Landscape Architects, Engineers, and Surveyors'
+  },
+  {
+    id: 188,
+    title: 'Mortgage Broker and Lender'
+  },
+  {
+    id: 189,
+    title: 'Brokerage Firm'
+  },
+  {
+    id: 190,
+    title: 'Real Estate Services'
+  },
+  {
+    id: 191,
+    title: 'Church and Other Religious Organizations'
+  },
+  {
+    id: 192,
+    title: 'Appliance Store'
+  },
+  {
+    id: 193,
+    title: 'Arts, Crafts and Hobbies'
+  },
+  {
+    id: 194,
+    title: 'Book Store'
+  },
+  {
+    id: 195,
+    title: 'Cards, Promotional Products and Gifts'
+  },
+  {
+    id: 196,
+    title: 'Clothing and Accessories'
+  },
+  {
+    id: 197,
+    title: 'Consumer Electronics and Accessories'
+  },
+  {
+    id: 198,
+    title: 'Dry Cleaners and Laundromats'
+  },
+  {
+    id: 199,
+    title: 'Florists/flower Shops'
+  },
+  {
+    id: 200,
+    title: 'Music Store'
+  },
+  {
+    id: 201,
+    title: 'Office Supplies'
+  },
+  {
+    id: 202,
+    title: 'Optical Stores and Opticians'
+  },
+  {
+    id: 203,
+    title: 'Cell Phone Services'
+  },
+  {
+    id: 204,
+    title: 'Party Supplies'
+  },
+  {
+    id: 205,
+    title: 'Hardware Store'
+  },
+  {
+    id: 206,
+    title: 'Nail Salons'
+  },
+  {
+    id: 207,
+    title: 'Skin Care'
+  },
+  {
+    id: 208,
+    title: 'Martial Arts'
+  },
+  {
+    id: 209,
+    title: 'Yoga Studios'
+  },
+  {
+    id: 210,
+    title: 'Wholesale'
+  },
+  {
+    id: 211,
+    title: 'Beauty Supplies'
+  },
+  {
+    id: 212,
+    title: 'Utility Companies'
+  }
+];
